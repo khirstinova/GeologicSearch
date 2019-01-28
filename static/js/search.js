97ca0b794b8c2480f4002a976af6ea7c739bde76
khirstinova <khirst@inovasolutions.com>
2019-01-27 19:46:36 -0500

    1.) Tightened up "add" and "subtract" term UI.
    2.) Added search results container, and journal results UI
    3.) Fixed some of the random links.

--- static/js/search.js
+++ static/js/search.js
@@ -7,7 +7,30 @@ $(document).ready(
                 let n = $(this).attr('data-next');
                 let classname = '.search_' + n;
                 $(classname).show();
+                $(this).prop("disabled", true);
+                $(this).siblings('.close').hide();
             }
         );
+
+        $('#search-btn').click
+        (
+            function(e) {
+                e.preventDefault();
+                $('.search-results-container').show();
+                setTimeout(function () {
+                    $('.search-wait').hide();
+                    $('.search-results-list').show();
+                }, 10000);
+            }
+        );
+
+        $('.close').click(function() {
+            let p = $(this).parent();
+            $(this).siblings('input').val('');
+            let number = p.attr('class').replace("search_", "");
+            $('button[data-next=' + number + ']').prop("disabled", false);
+            $('.search_' + (number - 1)).find('.close').show();
+            p.hide();
+        });
     }
 );
